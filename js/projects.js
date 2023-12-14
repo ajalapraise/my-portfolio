09fa670fd7389c3cd6e86bcb0faceb5e954bb461
ajalapraise <akintomiisin@gmail.com>
2023-12-14 02:54:14 +0100

    first commit

---
+++ js/projects.js
@@ -0,0 +1,101 @@
+const projects = [
+    {
+        id: 1,
+        image: "/images/getlinked.png",
+        name: "Get Linked App (twitter challenge)",
+        github: "https://github.com/ajalapraise/get-linked-challenge",
+        live: "https://get-linked-challenge.vercel.app/",
+        tools: ["Next JS", "Tailwind CSS", "Typescript", "API"]
+    },
+    {
+        id: 2,
+        image: "/images/gericht.png",
+        name: "Gericht (still building)",
+        github: "https://github.com/ajalapraise/gericht",
+        live: "https://my-gericht-app.vercel.app/",
+        tools: ["Next JS", "Tailwind CSS", "Typescript", "API"]
+    },
+    {
+        id: 3,
+        image: "/images/course-connekt.png",
+        name: "Course-connekt",
+        github: "https://github.com/ajalapraise/Course-Connekt",
+        live: "https://course-connekt.vercel.app/",
+        tools: ["React JS", "Tailwind CSS", "Typescript"]
+    },
+    {
+        id: 4,
+        image: "/images/ip-address.png",
+        name: "IP Address Tracker App",
+        github: "https://github.com/ajalapraise/ip-address-tracker",
+        live: "https://ip-address-tracker2.vercel.app/",
+        tools: ["HTML", "CSS", "Javascript", "API"]
+    },
+    {
+        id: 5,
+        image: "/images/expense-tracker.png",
+        name: "Expense Tracker App",
+        github: "https://github.com/ajalapraise/expense-tracker-react",
+        live: "https://expense-tracker-react-liart.vercel.app",
+        tools: ["React JS", "CSS", "Javascript"]
+    },
+    {
+        id: 6,
+        image: "/images/task-tracker.png",
+        name: "Task Tracker App",
+        github: "https://github.com/ajalapraise/react-task-tracker",
+        live: "https://react-task-tracker-ten.vercel.app/",
+        tools: ["React JS", "CSS", "Javascript"]
+    },
+    {
+        id: 7,
+        image: "/images/advice-generator.png",
+        name: "Advice Generator App",
+        github: "https://github.com/ajalapraise/advice-generator",
+        live: "https://advice-generator-woad.vercel.app/",
+        tools: ["HTML", "CSS", "Javascript", "API"]
+    },
+    {
+        id: 8,
+        image: "/images/feed-a-mole.png",
+        name: "Feed A Mole game",
+        github: "https://github.com/ajalapraise/feed-a-mole-game",
+        live: "https://feed-a-mole-game.netlify.app/",
+        tools: ["HTML", "CSS", "Javascript"]
+    },
+    {
+        id: 9,
+        image: "/images/card-generator.png",
+        name: "Virtual Card Marker App",
+        github: "https://github.com/ajalapraise/Interactive-card-",
+        live: "https://interactivecarddetail.netlify.app/",
+        tools: ["HTML", "CSS", "Javascript"]
+    },
+    {
+        id: 10,
+        image: "/images/book-store.png",
+        name: "Book Store App",
+        github: "https://github.com/ajalapraise/Book-store-app",
+        live: "https://book-storezz.netlify.app",
+        tools: ["HTML", "CSS", "Javascript", "Font Awesome"]
+    },
+    {
+        id: 11,
+        image: "/images/todo-list.png",
+        name: "Todo List App",
+        github: "https://github.com/ajalapraise/Todo-list-app-update-",
+        live: "https://bravo.netlify.app/",
+        tools: ["HTML", "CSS", "Javascript", "SCSS", "Font Awesome"]
+    },
+    {
+        id: 12,
+        image: "/images/calculator.png",
+        name: "Calculator App",
+        github: "https://github.com/ajalapraise/Calculator",
+        live: "https://mycalcccc.netlify.app/",
+        tools: ["HTML", "CSS", "Javascript"]
+    },
+
+];
+
+export default projects;
